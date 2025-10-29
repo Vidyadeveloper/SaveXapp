63e36d170b109602dfd6bad2bb67a5e2bfa34414
Vidyadeveloper <vidyabharti.sahu@swiftrinity.com>
2025-10-29 17:14:08 +0100

    repo

---
+++ db.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql2");
+
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: process.env.DB_SSL === "true" ? {rejectUnauthorized: false} : undefined,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("❌ Error connecting to MySQL:", err);
+  } else {
+    console.log("✅ Connected to MySQL Database");
+  }
+});
+
+module.exports = connection;
